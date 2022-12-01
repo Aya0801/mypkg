2dc6a3384a504e6eba6d959dada2b9e447eb2d52
Atsuya Sawayama <s21c1058fk@s.chibakoudai.jp>
2022-12-01 12:22:11 +0900

    change

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,18 +1,9 @@
-import rclpy                     
-from rclpy.node import Node      
-from person_msgs.msg import Person   
+
+import rclpy                     #ROS2のクライアントのためのライブラリ
+from rclpy.node import Node      #ノードを実装するためのNodeクラス（クラスは第10回で）
+from std_msgs.msg import Int16   #通信の型（16ビットの符号付き整数）
 
 rclpy.init()
-node = Node("talker")            
-pub = node.create_publisher(Person, "person", 10)   
-n = 0 
-def cb():          
-    global n       
-    msg = Person()  
-    msg.name = "上田隆一"
-    msg.age = n
-    pub.publish(msg)        
-    n += 1
-    
-node.create_timer(0.5, cb)  
-rclpy.spin(node)
+node = Node("talker")            #ノード作成（nodeという「オブジェクト」を作成）
+pub = node.create_publisher(Int16, "countup", 10)   #パブリッシャのオブジェクト作成
+n = 0 #カウント用変数
