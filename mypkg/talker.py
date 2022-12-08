5a5d3d49b596c8c30780b54af732be6b182525a7
Atsuya Sawayama <s21c1058fk@s.chibakoudai.jp>
2022-12-08 12:44:43 +0900

    talker

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -2,16 +2,19 @@ import rclpy
 from rclpy.node import Node      
 from std_msgs.msg import Int16   
 
+class Talker():
+    def __init__(self, node):
+        self.pub = node.create_publisher(Int16, "countup", 10)
+        self.n = 0
+        node.create_timer(0.5, self.cb)
+ 
+    def cb(self):
+        msg = Int16()
+        msg.data = self.n     
+        self.pub.publish(msg) 
+        self.n += 1
+
 rclpy.init()
-node = Node("talker")            
-pub = node.create_publisher(Int16, "countup", 10)   
-n = 0 
-def cb():          #17行目で定期実行されるコールバック関数
-    global n       #関数を抜けてもnがリセットされないようにしている
-    msg = Int16()  #メッセージの「オブジェクト」
-    msg.data = n   #msgオブジェクトの持つdataにnを結び付け
-    pub.publish(msg)        #pubの持つpublishでメッセージ送信
-    n += 1
-                      
-node.create_timer(0.5, cb)  
-rclpy.spin(node)       
+node = Node("talker")
+talker = Talker(node) 
+rclpy.spin(node)
