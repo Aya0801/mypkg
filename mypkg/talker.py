481ae7c97224c6d10ac9435ed8b89a10b1689fdc
Atsuya Sawayama <s21c1058fk@s.chibakoudai.jp>
2022-12-23 11:56:11 +0900

    add a command

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,3 +1,6 @@
+# SPDX -FileCopyrightText:2022 Atsuya Sawayama
+# SPDX-License-Identifier: BSD-3-Clause
+
 import rclpy                     
 from rclpy.node import Node      
 from person_msgs.msg import Person   
